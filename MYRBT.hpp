010d488061b68f0a9e6b1105ea616cc720db030b
Jabberwocky <wangy5236@gmail.com>
2024-07-11 18:20:10 +0800

    new file:   MYRBT.hpp

---
+++ MYRBT.hpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+template <class K>
+class Node
+{
+private:
+    int color;
+    K key;
+    Node *left;
+    Node *right;
+    Node *parent;
+
+public:
+    bool setColor(int color)
+    {
+        this->color = color;
+        return true;
+    }
+    bool setKey(K key)
+    {
+        this->key = key;
+        return true;
+    }
+    bool setLeft(Node *left)
+    {
+        this->left = left;
+        return true;
+    }
+    bool setRight(Node *Right)
+    {
+        this->Right = Right;
+        return true;
+    }
+    bool setParent(Node *Parent)
+    {
+        this->Parent = Parent;
+        return true;
+    }
+    int getColor()
+    {
+        return this->color;
+    }
+    K getKey()
+    {
+        return this->key;
+    }
+    Node *getLeft()
+    {
+        return this->left;
+    }
+    Node *getRight()
+    {
+        return this->right;
+    }
+    Node *getParent()
+    {
+        return this->parent;
+    }
+};
+template <class K>
+class MYRBT
+{
+private:
+    Node<K> *root;
+    Node<K> *Nil;
+
+public:
+    MYRBT()
+    {
+    }
+};
